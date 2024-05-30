161f7c0ebabaa7817d50b3e40760f7ead58b6c9a
fabio-walkway <fabio@walkway.ai>
2024-05-30 10:50:45 +0000

    new config

--- configure.sh
+++ configure.sh
@@ -2,28 +2,26 @@
 
 set -e
 
+sudo apt-get update
+
 echo "------------------------------INSTALLING DOCKER------------------------------"
 echo "-----------------------------------------------------------------------------"
-sudo apt install docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo usermod -aG docker $USER
 
 echo "------------------------------INSTALLING KUBECTL-----------------------------"
 echo "-----------------------------------------------------------------------------"
-sudo apt-get install kubectl
+sudo apt-get install kubectl -y
 
-echo "-------------------------------INSTALLING KIND-------------------------------"
+echo "-------------------------------INSTALLING MINIKUBE-------------------------------"
 echo "-----------------------------------------------------------------------------"
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-arm64
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube /usr/local/bin/
 
 echo "----------------------SETTING UP KUBERNETES CLUSTER--------------------------"
 echo "-----------------------------------------------------------------------------"
-ip_addresses=$(hostname -I)
-export MY_IP_ADDRESS=$(echo "$ip_addresses" | awk '{print $1}')
-kind delete cluster --name kind
-envsubst < kubernetes/cluster.yaml | kind create cluster --retain --config=-
-kubectl cluster-info --context kind-kind
+minikube start
 
 #admin/[kubectl exec -it svc/jenkins bash][cat /run/secrets/additional/chart-admin-password]
 echo "----------------------------INSTALLING JENKINS-------------------------------"
@@ -36,7 +34,7 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-helm install jenkins jenkins/jenkins --set controller.resources.requests.memory=16Gi --set controller.resources.limits.memory=48Gi
+helm install jenkins jenkins/jenkins --set controller.resources.requests.memory=8Gi --set controller.resources.limits.memory=12Gi
 kubectl apply -f kubernetes/jenkins-token.yaml
 echo "------------------------------BEGINNING TOKEN--------------------------------"
 kubectl describe secret $(kubectl describe serviceaccount jenkins | grep token | awk '{print $2}')
@@ -50,7 +48,6 @@ kubectl apply -f kubernetes/walkwayai-configmap.yaml
 kubectl apply -f kubernetes/walkwayai-secret.yaml
 kubectl apply -f kubernetes/mongodb.yaml
 sleep 20
-export CLUSTER_NODE_ID=$(kubectl get node -o wide | awk 'NR==2 {print $6}')
 export TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 envsubst < infra-config.yaml > infra-config-pipeline.yaml
 git add .
