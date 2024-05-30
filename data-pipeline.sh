4859ec9f33fd7a7554acef8b68822c4cd0997b58
fabio-walkway <fabio@walkway.ai>
2024-05-30 10:20:31 +0000

    removed nodeport for mongodb

--- data-pipeline.sh
+++ data-pipeline.sh
@@ -29,6 +29,6 @@ python3 src/text_summarization.py
 python3 src/embed_textual_data.py
 python3 src/generate_final_embeddings.py
 python3 src/find_similarity_product.py
-python3 src/send_data_mongodb
+python3 src/send_data_mongodb.py
 
 deactivate
