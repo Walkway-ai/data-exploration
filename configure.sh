ee219b5f9e0ca1543360dde8fe66851517c8c8df
fabio-walkway <fabio@walkway.ai>
2024-05-30 12:55:52 +0000

    save

--- configure.sh
+++ configure.sh
@@ -8,46 +8,49 @@ echo "------------------------------INSTALLING DOCKER---------------------------
 echo "-----------------------------------------------------------------------------"
 sudo apt install docker.io -y
 sudo systemctl start docker
-sudo usermod -aG docker $USER
-
 echo "------------------------------INSTALLING KUBECTL-----------------------------"
 echo "-----------------------------------------------------------------------------"
 sudo apt-get install kubectl -y
-
-echo "-------------------------------INSTALLING MINIKUBE-------------------------------"
+echo "-------------------------------INSTALLING KIND-------------------------------"
 echo "-----------------------------------------------------------------------------"
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube /usr/local/bin/
-
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-arm64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
 echo "----------------------SETTING UP KUBERNETES CLUSTER--------------------------"
 echo "-----------------------------------------------------------------------------"
-minikube start
-
-#admin/[kubectl exec -it svc/jenkins bash][cat /run/secrets/additional/chart-admin-password]
-echo "----------------------------INSTALLING JENKINS-------------------------------"
-echo "--------------------------https://localhost:8080-----------------------------"
-kubectl create namespace jenkins
+ip_addresses=$(hostname -I)
+export MY_IP_ADDRESS=$(echo "$ip_addresses" | awk '{print $1}')
+kind delete cluster --name kind
+envsubst < kubernetes/cluster.yaml | kind create cluster --retain --config=-
+kubectl cluster-info --context kind-kind
+echo "-------------------------------INSTALLING KUBENS-------------------------------"
+echo "-----------------------------------------------------------------------------"
 sudo apt-get install kubectx
-kubens jenkins
+echo "-------------------------------INSTALLING HELM-------------------------------"
+echo "-----------------------------------------------------------------------------"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+echo "----------------------------INSTALLING JENKINS-------------------------------"
+echo "-----------------------------------------------------------------------------"
+#admin/[kubectl exec -it svc/jenkins bash][cat /run/secrets/additional/chart-admin-password]
+kubectl create namespace jenkins
+kubens jenkins
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-helm install jenkins jenkins/jenkins --set controller.resources.requests.memory=8Gi --set controller.resources.limits.memory=12Gi
+helm install jenkins jenkins/jenkins --set controller.resources.requests.memory=16Gi --set controller.resources.limits.memory=48Gi
 kubectl apply -f kubernetes/jenkins-token.yaml
 echo "------------------------------BEGINNING TOKEN--------------------------------"
 kubectl describe secret $(kubectl describe serviceaccount jenkins | grep token | awk '{print $2}')
 echo "---------------------------------END TOKEN-----------------------------------"
 kubectl create rolebinding jenkins-admin-binding --clusterrole=admin --serviceaccount=jenkins:jenkins
-
 echo "----------------------------INSTALLING MONGODB-------------------------------"
-echo "--------------------------https://localhost:27017----------------------------"
-echo "----------------------http://${CLUSTER_NODE_ID}:27017------------------------"
 kubectl apply -f kubernetes/walkwayai-configmap.yaml
 kubectl apply -f kubernetes/walkwayai-secret.yaml
 kubectl apply -f kubernetes/mongodb.yaml
 sleep 20
+export CLUSTER_NODE_ID=$(kubectl get node -o wide | awk 'NR==2 {print $6}')
 export TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 envsubst < infra-config.yaml > infra-config-pipeline.yaml
 git add .
