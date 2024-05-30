5af9a55b4eb9c891ed107ecb8b0814e1c976dca7
fabio-walkway <fabio@walkway.ai>
2024-05-30 20:40:10 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,18 +2,31 @@
 
 set -e
 
-gcloud auth login
-gcloud config set project "ww-da-ingestion"
+# Set up GCP CLI
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-478.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-478.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
+
+# Install Plugin
+gcloud components install gke-gcloud-auth-plugin
+
+# Set local KUBECONFIG
 gcloud container clusters create walkway-cluster --num-nodes=1 --zone=us-central1-c
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 gcloud container clusters get-credentials walkway-cluster --zone us-central1-c
 
-# Install Jenkins
+kubectl create namespace jenkins
 helm repo add jenkins https://charts.jenkins.io
-helm install jenkins jenkins/jenkins --namespace jenkins --set service.type=LoadBalancer
+helm repo update
+
+# Install Jenkins
+helm install jenkins jenkins/jenkins
+kubectl patch svc jenkins -n jenkins --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
+gcloud compute firewall-rules create allow-jenkins-nodeport --allow=tcp:30429 --description="Allow traffic to Jenkins NodePort" --direction=INGRESS --target-tags=all-instances --priority=1000
 
 # Install MongoDB
-helm install mongodb bitnami/mongodb --namespace mongodb
+helm install mongodb bitnami/mongodb
 
 # Install Mongo Express
-helm install mongo-express bitnami/mongodb --namespace mongodb
+helm install mongo-express bitnami/mongodb
