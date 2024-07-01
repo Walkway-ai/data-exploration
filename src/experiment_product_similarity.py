89e86f0c9cec32feb63ce45213c63966c460e571
fabiocuri <fcuri91@gmail.com>
2024-07-01 16:04:50 +0100

    added new filter

--- src/experiment_product_similarity.py
+++ src/experiment_product_similarity.py
@@ -56,8 +56,6 @@ def query_gpt(apikey, text_field, df, df_product):
     df = df.astype(str)
     df_product = df_product.astype(str)
 
-    del df_product["Category"]
-
     product_features = "\n".join(
         [f"{col}: {list(df_product[col])[0]}" for col in list(df_product.columns)]
     )
@@ -75,6 +73,8 @@ def query_gpt(apikey, text_field, df, df_product):
         candidates_str += "\n \n" + candidates_str_now
 
     prompt = f"Given the following REFERENCE PRODUCT, identify the PRODUCTCODEs of any POSSIBILITY PRODUCTS that are extremely similar to it. Similarity should be determined based on the content of {text_field}, and similar products include the same activities (e.g. a tour in the same place, or the same activity). \nREFERENCE PRODUCT: \n \n{product_features} \n \nPOSSIBILITY PRODUCTS: {candidates_str} \n \nYour answer should contain ONLY a Python list of the PRODUCTCODEs of the similar products (e.g., ['18745FBP', 'H73TOUR2']). If there are no similar products, return an empty list ([])."
+
+    print(prompt)
 
     client = OpenAI(api_key=apikey)
 
