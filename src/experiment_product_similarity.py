ba78cd25bfcf310ac6040cfd67f11e8ad55037cd
fabiocuri <fcuri91@gmail.com>
2024-07-01 16:07:06 +0100

    added new filter

--- src/experiment_product_similarity.py
+++ src/experiment_product_similarity.py
@@ -74,8 +74,6 @@ def query_gpt(apikey, text_field, df, df_product):
 
     prompt = f"Given the following REFERENCE PRODUCT, identify the PRODUCTCODEs of any POSSIBILITY PRODUCTS that are extremely similar to it. Similarity should be determined based on the content of {text_field}, and similar products include the same activities (e.g. a tour in the same place, or the same activity). \nREFERENCE PRODUCT: \n \n{product_features} \n \nPOSSIBILITY PRODUCTS: {candidates_str} \n \nYour answer should contain ONLY a Python list of the PRODUCTCODEs of the similar products (e.g., ['18745FBP', 'H73TOUR2']). If there are no similar products, return an empty list ([])."
 
-    print(prompt)
-
     client = OpenAI(api_key=apikey)
 
     result = client.chat.completions.create(
