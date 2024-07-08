d41e4b6f2458aee104706848c4f780815d6359ba
fabiocuri <fcuri91@gmail.com>
2024-07-08 13:18:36 +0100

    save

--- deleteme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 src/generate_product_similarity.py --overwrite --embedding_model 'mean/mean' --embedding_fields description_inclexcl
-python3 src/generate_product_similarity.py --overwrite --embedding_model 'mean/mean' --embedding_fields description_title
-python3 src/generate_product_similarity.py --overwrite --embedding_model 'mean/mean' --embedding_fields title_inclexcl_tgdescription
-python3 src/generate_product_similarity.py --overwrite --embedding_model 'mean/mean' --embedding_fields title_inclexcl_tgdescription_description
